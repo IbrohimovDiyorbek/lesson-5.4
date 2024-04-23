34ed7e3161fb712fe8c77e1bd7f3c1a11d374c6d
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-04-23 08:43:56 +0500

    it is a homework

---
+++ main.js
@@ -0,0 +1,16 @@
+const title = document.querySelector('body')
+const subtitle = document.querySelector('h1')
+let random = Math.floor(Math.random()* 100)
+title.style.background = 'black'
+title.style.display = 'flex'
+title.style.justifyContent = 'center'
+title.style.marginTop = '250px'
+title.style.fontSize = '50px'
+title.style.fontFamily = 'DynaPuff'
+
+
+
+
+subtitle.style.color = 'red';
+
+subtitle.innerHTML = random;
